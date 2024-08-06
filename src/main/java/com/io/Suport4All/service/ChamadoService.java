582004de44c92d3554b391f86a70290d4bb7e18a
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-08-06 08:31:56 -0300

    Criação do ChmadoService e de seu repository

---
+++ src/main/java/com/io/Suport4All/service/ChamadoService.java
@@ -0,0 +1,37 @@
+package com.io.Suport4All.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.io.Suport4All.repository.ChamadoRepository;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	
+	//Encontrar um chamado por ID
+	
+	
+	
+	//Encontrar todos os chamados criados
+	
+	
+	
+	//Criar um chamado associando a um usuario
+	
+	
+	
+	//Atualizar um chamado associando a um usuario e passando o id do chamado
+	//Nota, não deve ser possivel alterar nada além dos status do chamado
+	
+	
+	//Deletar chamado? Acredito que isso não deve ser implementado segundo os requisitos do sistema que será feito
+	//Por questões de auditoria
+	
+
+	
+	
+}
