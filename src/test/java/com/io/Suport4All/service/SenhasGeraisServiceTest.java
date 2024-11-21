ca8d863586f663ee2df9a3b7ff70ca189d342688
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-11-21 10:02:32 -0300

    Inicio dos testes unitarios em senhasGerais

--- src/test/java/com/io/Suport4All/service/SenhasGeraisServiceTest.java
+++ src/test/java/com/io/Suport4All/service/SenhasGeraisServiceTest.java
@@ -1,12 +1,17 @@
 package com.io.Suport4All.service;
 
+import static org.mockito.Mockito.mock;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.io.Suport4All.dto.SenhasGeraisDto;
+import com.io.Suport4All.entity.SenhasGeraisEntity;
 import com.io.Suport4All.repository.SenhasGeraisRepository;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SenhasGeraisServiceTest {
@@ -17,8 +22,27 @@ public class SenhasGeraisServiceTest {
 	@InjectMocks
 	private SenhasGeraisService senhaService;
 	
+	
+//	senhaEntity.setLogin(senhaDto.getLogin());
+//	senhaEntity.setDescricao(senhaDto.getDescricao());
+//	senhaEntity.setEmail(senhaDto.getEmail());
+//	senhaEntity.setOrigem(senhaDto.getOrigem());
+//	senhaEntity.setSenha(senhaDto.getSenha());
 	@Test
 	public void deveCriarSenha() {
+		
+		var senhaEntity = mock(SenhasGeraisDto.class);
+		
+		given(senhaEntity.getId()).willReturn(1L);
+		given(senhaEntity.getLogin()).willReturn("tiLi");
+		given(senhaEntity.getDescricao()).willReturn("Local onde se encontra os registros dos dominios da empresa!");
+		given(senhaEntity.getEmail()).willReturn("ti@gmail.com");
+		given(senhaEntity.getOrigem()).willReturn("RegistroBr");
+		given(senhaEntity.getSenha()).willReturn("14512");
+		
+//		given(senhaRepository.save(senhaEntity)).willReturn(senhaEntity);
+//
+//		var result = senhaService.createSenha(senhaEntity);
 		
 	}
 	
